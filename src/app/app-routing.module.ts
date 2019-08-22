35e24f8c2795b1c7c81a0ff3739798d1e9eaa19d
Gizelly Steffanny <gizellysteffanny@gmail.com>
2019-08-22 19:23:53 -0400

    create model main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AccessControlRoutingModule } from './modules/access-control/access-control-routing.module';
+import { MainRoutingModule } from './modules/main/main-routing.module';
 
 
 
@@ -12,7 +13,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    AccessControlRoutingModule
+    AccessControlRoutingModule,
+    MainRoutingModule
   ],
   exports: [RouterModule]
 })
